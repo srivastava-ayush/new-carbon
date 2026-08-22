b08785aa95f416a41d25f27a1b53b8f318691ccd
iabhiijeet <abhijeetrao7800@gmail.com>
2026-08-22 23:26:47 +0530

    feat: comment out authentication logic in signIn and signUp actionshanges f

--- src/app/auth/signup/action.ts
+++ src/app/auth/signup/action.ts
@@ -6,20 +6,20 @@ export async function signUpWithEmail(
   formData: FormData
 ) {
   const email = formData.get('email') as string;
-  if (!email) {
-    return { error: "Email address must be provided." }
-  }
+  // if (!email) {
+  //   return { error: "Email address must be provided." }
+  // }
   // Optionally restrict sign ups based on email address
   // if (!email.trim().endsWith("@my-company.com")) {
   //  return { error: 'Email must be from my-company.com' };
   // }
-  const { error } = await auth.signUp.email({
-    email,
-    name: formData.get('name') as string,
-    password: formData.get('password') as string,
-  });
-  if (error) {
-    return { error: error.message || 'Failed to create account' };
-  }
+  // const { error } = await auth.signUp.email({
+  //   email,
+  //   name: formData.get('name') as string,
+  //   password: formData.get('password') as string,
+  // });
+  // if (error) {
+  //   return { error: error.message || 'Failed to create account' };
+  // }
   redirect('/dashboard');
 }
